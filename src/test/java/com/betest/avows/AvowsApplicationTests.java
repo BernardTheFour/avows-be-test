162b9cb72df175ac4f3c72237dd4f071cf35230f
BernardTheFour <bernardctxy@gmail.com>
2024-07-12 13:26:33 +0700

    Spring initialzr init

---
+++ src/test/java/com/betest/avows/AvowsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.betest.avows;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AvowsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
