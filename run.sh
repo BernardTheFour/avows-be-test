2c7ac7f3a8309009d15a0e0f6af49cbed41d9d90
BernardTheFour <bernardctxy1@gmail.com>
2024-07-13 21:10:06 +0700

    change run sh back to default

--- run.sh
+++ run.sh
@@ -7,8 +7,8 @@
     export DB_NAME=demo
 
     mvn test 
-    # mvn liquibase:update
-    # mvn spring-boot:run -e
+    mvn liquibase:update
+    mvn spring-boot:run -e
 
     # generate db migration
     # mvn liquibase:diff -Dmaven.test.skip=true 
